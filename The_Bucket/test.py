67a40bd725b31a77d8e34b777540a31cd3819ad8
rbcommoncents <rbcommoncents@gmail.com>
2025-02-06 22:27:46 -0500

    Automated commit

--- The_Bucket/test.py
+++
@@ -1,4 +0,0 @@
-text = input("Enter up to 255 characters: ")[:255]
-pattern = input("Enter the pattern to replace: ")
-replacement = input("Enter the replacement: ")
-print(text.replace(pattern, replacement))
